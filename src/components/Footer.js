ff75555698240b33041ab77be6b45c1ec7e6f887
Valeria Granada Rodas <55161707+vale0722@users.noreply.github.com>
2020-05-27 23:24:09 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,23 +4,31 @@ import { Header } from '../components/Header'
 
 const useStyles = makeStyles({
   footer: {
+    backgroundColor: '#F8F8F8',
+    textAlign: 'center',
+    position: 'fixed',
+    bottom: 23,
+    height: 60,
+    width: '100%',
     borderTop: '1px solid',
-    position: 'sticky',
-    bottom: 0,
-    right: 0,
-    left: 0,
     color: 'white',
     webkitBoxShadow: '2px -1px 21px -1px rgba(189,185,189,1)',
     mozBoxShadow: '2px -1px 21px -1px rgba(189,185,189,1)',
     boxShadow: '2px -1px 21px -1px rgba(189,185,189,1)'
+  },
+  phantom: {
+    display: 'block',
+    width: '100%'
   }
 })
 
 export function Footer () {
   const classes = useStyles()
   return (
-    <footer className={classes.footer}>
-      <Header />
-    </footer>
+    <div className={classes.phantom}>
+      <footer className={classes.footer}>
+        <Header />
+      </footer>
+    </div>
   )
 }
